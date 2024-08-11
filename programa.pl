36b95a9e7bf8c540f48461bea9d1b076e9a9f8ec
Reichel-eze <reicheleze@gmail.com>
2024-08-10 22:37:49 -0300

    QUASI TERMINADO

--- programa.pl
+++ programa.pl
@@ -39,6 +39,10 @@ nivelSuperior(seguridad, fisiologico).
 % entre empleo y salud es 0, y la separación entre respiración y 
 % confianza es 3.
 
+% LA COMPARACION LA PUEDE HACER DE ABAJO HACIA ARRIBA 
+% separacionDeNecesidades(respiracion, confianza, 3). TRUE
+% separacionDeNecesidades(confianza, respiracion, 3). FALSE
+
 separacionDeNecesidades(NecesidadA, NecesidadB, Separacion) :-
     necesidad(NecesidadA, NivelA),
     necesidad(NecesidadB, NivelB),
@@ -64,7 +68,7 @@ separacionDeNiveles(NivelA, NivelB, Separacion) :-
 % NECESIDADES SIN SATISFACER
 
 necesita(carla, alimentacion).
-necesita(carla, descansar).
+necesita(carla, descanso).
 necesita(carla, empleo).
 
 necesita(juan, afecto).
@@ -81,12 +85,23 @@ necesita(charly, afecto).
 
 necesidadConMayorJerarquia(Persona, NecesidadMayor) :-
     necesita(Persona, NecesidadMayor),
-    forall(necesita(Persona, _), mayorNecesidad(NecesidadMayor, _)).
+    forall((necesita(Persona, OtraNecesidad), OtraNecesidad \= NecesidadMayor), mayorNecesidad(NecesidadMayor, OtraNecesidad)).
 
 necesidadConMayorJerarquiaNOT(Persona, NecesidadMayor) :-
     necesita(Persona, NecesidadMayor),
-    not((necesita(Persona, OtraNecesidad), mayorNecesidad(OtraNecesidad, NecesidadMayor))).
+    not((necesita(Persona, OtraNecesidad), OtraNecesidad \= NecesidadMayor, mayorNecesidad(OtraNecesidad, NecesidadMayor))).
+
+necesidadConMayorJerarquiaV2(Persona, NecesidadMayor) :-
+    necesita(Persona, NecesidadMayor),
+    not((necesita(Persona, OtraNecesidad), mayorJerarquia(OtraNecesidad, NecesidadMayor))).
+    % NO existe OtraNecesidad que tenga mayor jerarquia que la Necesidad Mayor
     
+mayorJerarquia(Necesidad, OtraNecesidad) :-
+    separacionDeNecesidades(OtraNecesidad, Necesidad, Separacion),
+    Separacion > 0.
+
+% LO QUE HICE ACA ABAJO ESTA BIEN PERO CON EL PREDICADO DE ARRIBA ME AHORRO ESTO...
+
 mayorNecesidad(NecesidadMayor, NecesidadMenor) :-
     necesidad(NecesidadMayor, NivelA),
     necesidad(NecesidadMenor, NivelB),
@@ -101,6 +116,61 @@ mayorNivel(NivelSuperior, NivelInferior) :-
 
 % 5) Saber si una persona pudo satisfacer por completo algún nivel de la pirámide.
 % Por ejemplo, Juan pudo satisfacer por completo el nivel fisiologico.
+
+nivel(Nivel) :- necesidad(_, Nivel).
+persona(Persona) :- necesita(Persona, _).
+
+nivelSatisfecho(Persona, Nivel) :-
+    nivel(Nivel),
+    persona(Persona),
+    not(nivelInsatisfecho(Persona, Nivel)).
+
+nivelInsatisfecho(Persona, Nivel) :-
+    necesita(Persona, Necesidad),
+    necesidad(Necesidad, Nivel).
+    
+nivelInsatisfechoV2(Persona, Nivel) :-
+    necesita(Persona, Necesidad),
+    necesidad(Necesidad, OtroNivel),
+    separacionDeNiveles(OtroNivel,Nivel,_).
+
+% Teoria Maslow --> En base a eso, uno de sus elementos centrales de es que las personas sólo atienden 
+% necesidades superiores cuando han satisfecho las necesidades inferiores. Por ejemplo, las necesidades de
+% seguridad surgen cuando las necesidades fisiológicas están satisfechas. 
+
+% 6) Definir los predicados que permitan analizar si es cierta o no la teoría de Maslow:
+% a) Para una persona en particular.
+% b) Para todas las personas.
+% c) Para la mayoría de las personas. 
+
+% Nota: existen los predicados de aridad 0. Por ejemplo: 
+% noLlegoElFinDelMundo :- vive(Alguien).
+
+% cumpleMaslow --> cuando todas las necesidades son del mismo nivel
+
+cumpleMaslow(Persona) :-
+    necesita(Persona, Necesidad),
+    forall(necesita(Persona, OtraNecesidad), mismoNivel(Necesidad, OtraNecesidad)).
+
+mismoNivel(Necesidad, OtraNecesidad) :- separacionDeNecesidades(Necesidad, OtraNecesidad, 0).
+
+noCumpleMaslow(Persona) :-
+    persona(Persona),
+    necesita(Persona, Necesidad),
+    necesita(Persona, OtraNecesidad),
+    Necesidad \= OtraNecesidad,
+    separacionDeNecesidades(Necesidad, OtraNecesidad, Separacion),
+    Separacion >= 1.
+
+cumpleMaslowTODOS :- forall(persona(Persona), cumpleMaslow(Persona)).
+
+
+
+
+
+
+
+
 
 
 
